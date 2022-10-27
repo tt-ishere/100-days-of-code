56d185b0e3cae3fe8e23e1a35759c3476299af0a
bizzy-Code <63963271+bizzy-Code@users.noreply.github.com>
2022-10-27 09:28:02 +0000

    Day 23 update

---
+++ Day 23 - Intermediate - The Turtle Crossing Capstone Project/car_manager.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+import random
+
+COLORS = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black']
+MOVE_DISTANCE = 5
+car = []
+
+class CarManager():
+    def __init__(self):
+        self.all_cars = []
+        self.speed = MOVE_DISTANCE
+    
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle(shape='square')
+            new_car.color(random.choice(COLORS))
+            new_car.shapesize(stretch_len=2, stretch_wid=1)
+            new_car.penup()
+            random_car_y = random.randint(-240, 250)
+            new_car.goto(300, random_car_y)
+            self.all_cars.append(new_car)
+
+    def move(self):
+        for car in self.all_cars:
+            car.backward(self.speed)
+    
+    def level_up(self):
+        self.speed += MOVE_DISTANCE 
